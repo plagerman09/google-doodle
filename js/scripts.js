3f6114bbd9f3687d319754e8d35e4a3da46dadb2
plagerman09 <jplagerman1@eagles.ewu.edu>
2020-05-14 08:55:14 -0700

    Version 1
    Set up the first defaut version

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 console.log('OK');
 
-gsap.from( '#heart1', {
- 
-    x: -600,
-    fill: 'orange',
+// gsap.from( 
+//   WHAT , 
+//   {HOW
+
+//   });
+
+gsap.from('#heart1' , {
     opacity: 0,
-    duration: 3
+    duration: 5,
+    y: 5
+  }
+);
+
+// gsap.from( '#heart1', {
+ 
+//     x: -600,
+//     fill: 'orange',
+//     opacity: 0,
+//     duration: 3
     
-  });
+//   });
